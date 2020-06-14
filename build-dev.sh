b180990f2984070b65d1c13048300a51127f86a5
Joose Helle <joose.helle@gmail.com>
2020-06-14 12:13:29 +0300

    Update build-dev.sh

--- build-dev.sh
+++ build-dev.sh
@@ -4,11 +4,9 @@ set -ex
 
 USER='hellej'
 
-DOCKER_IMAGE=${USER}/hope-green-path-server:dev
-DOCKER_IMAGE_LATEST=${USER}/hope-green-path-server:latest
+for TAG in latest dev; do
+  DOCKER_IMAGE=${USER}/hope-green-path-server:${TAG}
 
-docker build -t ${DOCKER_IMAGE} .
-
-docker tag ${DOCKER_IMAGE} ${DOCKER_IMAGE_LATEST}
-docker login
-docker push ${DOCKER_IMAGE_LATEST}
+  docker build -t ${DOCKER_IMAGE} .
+  docker push ${DOCKER_IMAGE}
+done
