b70ecf3f27a1378809ee3fdb55f1f30cd09cb8d5
Joose Helle <joose.helle@gmail.com>
2020-06-14 14:21:03 +0300

    Parameterize worker count in startup

--- src/start-application.sh
+++ src/start-application.sh
@@ -1,1 +1,9 @@
-gunicorn --workers=1 --bind=0.0.0.0:5000 --log-level=info --timeout 160 green_paths_app:app
+#!/bin/bash
+
+if [[ -z "${WORKER_COUNT}" || "${WORKER_COUNT}" = "1" ]]; then
+  echo "Starting green path server with only 1 worker (default/dev)"
+  gunicorn --workers=1 --bind=0.0.0.0:5000 --log-level=info --timeout 160 green_paths_app:app
+else
+  echo "Starting green path server with ${WORKER_COUNT} workers (prod)"
+  gunicorn --workers=${WORKER_COUNT} --bind=0.0.0.0:5000 --log-level=warning --timeout 160 green_paths_app:app
+fi
