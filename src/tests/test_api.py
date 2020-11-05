4af403dee92fed1ac4d7699afa5c2bdfbad1152d
Joose Helle <joose.helle@gmail.com>
2020-11-05 18:41:13 +0200

    Fix test env

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,20 +1,32 @@
 
 import os
 import json
-from green_paths_app import app
+import pytest
 
 
-os.environ['GRAPH_SUBSET'] = 'True'
-client = app.test_client()
+@pytest.fixture
+def set_test_env(monkeypatch):
+    monkeypatch.setenv('GRAPH_SUBSET', 'True')
 
 
-def test_endpoint():
+@pytest.fixture
+def client(set_test_env):
+    from green_paths_app import app
+    with app.test_client() as client:
+        yield client
+
+
+def test_env(set_test_env):
+    assert os.getenv('GRAPH_SUBSET') == 'True'
+
+
+def test_endpoint(client):
     response = client.get('/')
     response.get_data().decode('utf-8') == 'Keep calm and walk green paths.'
     assert response.status_code == 200
 
 
-def test_aqi_status_path():
+def test_aqi_status_path(client):
     response = client.get('/aqistatus')
     assert response.status_code == 200
     data = json.loads(response.data)
@@ -22,7 +34,7 @@ def test_aqi_status_path():
     assert data['aqi_data_utc_time_secs'] == None
 
 
-def test_aqi_map_data_status_path():
+def test_aqi_map_data_status_path(client):
     response = client.get('/aqi-map-data-status')
     assert response.status_code == 200
     status = json.loads(response.data)
@@ -30,7 +42,7 @@ def test_aqi_map_data_status_path():
     assert status['aqi_map_data_utc_time_secs'] == None
 
 
-def test_aqi_map_data_path():
+def test_aqi_map_data_path(client):
     response = client.get('/aqi-map-data')
     assert response.status_code == 200
     # before first AQI map data load it should return just empty string
