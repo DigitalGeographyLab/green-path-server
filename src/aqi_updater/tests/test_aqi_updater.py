55d93f13cc0d48dd97688fafb0e0099a2f5ec1fc
Joose Helle <joose.helle@gmail.com>
2021-07-11 09:37:10 +0300

    Refactor tests

--- src/aqi_updater/tests/test_aqi_updater.py
+++ src/aqi_updater/tests/test_aqi_updater.py
@@ -24,24 +24,19 @@ def aqi_updater(graph):
     yield aqi_updater
 
 
-def test_extract_rt_aqi_from_zip():
+def test_extracts_aqi_nc_from_zip():
     zip_file = 'allPollutants_2021-02-26T14.zip'
     aq_file = aq_processing.extract_zipped_aq_file(test_data_dir, zip_file)
     assert aq_file == 'allPollutants_2021-02-26T14.nc'
 
 
-def test_convert_aqi_nc_to_tif():
+def test_converts_aqi_nc_to_tif():
     aq_nc_file = 'allPollutants_2021-02-26T14.nc'
     aq_tif = aq_processing.convert_aq_nc_to_tif(test_data_dir, aq_nc_file)
     assert aq_tif == 'aqi_2021-02-26T14.tif'
 
 
-def test_reads_scaled_aqi_tif():
-    aqi_raster = rasterio.open(test_data_dir + 'aqi_2020-10-10T08.tif')
-    assert not aq_processing._has_unscaled_aqi(aqi_raster)
-
-
-def test_converted_aqi_tif_has_valid_aqi():
+def test_converted_aqi_tif_has_valid_unscaled_aqi():
     aq_tif = 'aqi_2021-02-26T14.tif'
     aqi_filepath = test_data_dir + aq_tif
     aqi_raster = rasterio.open(aqi_filepath)
@@ -53,9 +48,10 @@ def test_converted_aqi_tif_has_valid_aqi():
     assert round(float(np.min(aqi_band_scaled)), 2) == 1.0
     assert round(float(np.max(aqi_band_scaled)), 2) == 2.89
     assert round(float(np.median(aqi_band_scaled)), 2) == 1.72
+    aqi_raster.close()
 
 
-def test_scales_and_offsets_raw_aqi_values():
+def test_scales_and_offsets_raster_values_to_aqi():
     aq_tif = 'aqi_2021-02-26T14.tif'
     aqi_filepath = test_data_dir + aq_tif
     aqi_raster = rasterio.open(aqi_filepath)
@@ -66,9 +62,10 @@ def test_scales_and_offsets_raw_aqi_values():
     aqi_band = aqi_raster.read(1)
     assert round(float(np.min(aqi_band)), 2) == 1.0
     assert round(float(np.max(aqi_band)), 2) == 2.89
+    aqi_raster.close()
 
 
-def test_fillna_in_aqi_raster():
+def test_fills_na_values_in_aqi_raster():
     aq_tif = 'aqi_2021-02-26T14.tif'
     aqi_filepath = test_data_dir + aq_tif
     aq_processing.fix_aqi_tiff_scale_offset(aqi_filepath)
@@ -77,7 +74,6 @@ def test_fillna_in_aqi_raster():
     nodata_count = np.sum(aqi_band <= 1.0)
     aqi_raster.close()
     assert nodata_count == 297150
-
     assert aq_processing.fillna_in_raster(test_data_dir, aq_tif, na_val=1.0)
     aqi_raster = rasterio.open(aqi_filepath)
     aqi_band = aqi_raster.read(1)
@@ -86,13 +82,13 @@ def test_fillna_in_aqi_raster():
     assert nodata_count == 0
 
 
-def test_create_aqi_update_csv(aqi_updater):
+def test_creates_aqi_update_csv(aqi_updater):
     assert aqi_updater.latest_aqi_csv == 'aqi_2020-10-10T08.csv'
     aqi_update_df = pd.read_csv(fr'{aqi_updates_dir}aqi_2020-10-10T08.csv')
     assert len(aqi_update_df) == 16469
 
 
-def test_aqi_update_csv_data_ok():
+def test_aqi_update_csv_aqi_values_are_valid():
     aqi_update_df = pd.read_csv(fr'{aqi_updates_dir}aqi_2020-10-10T08.csv')
     assert aqi_update_df[E.id_ig.name].nunique() == 16469
     assert round(aqi_update_df[E.aqi.name].mean(), 3) == 1.684
@@ -104,7 +100,7 @@ def test_aqi_update_csv_data_ok():
     assert len(not_null_aqis) == 16469
 
 
-def test_aqi_map_json():
+def test_creates_aqi_map_json():
     with open(fr'{aqi_updates_dir}aqi_map.json') as f:
         aqi_map = json.load(f)
         assert len(aqi_map) == 1
